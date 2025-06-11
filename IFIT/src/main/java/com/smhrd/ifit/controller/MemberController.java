4ca7df6abb6caa1b3a73b31f9fa7b9956ed1d240
smhrd <smhrd@192.168.219.200>
2025-06-11 11:27:47 +0900

    0611/진성 - 프로젝트 생성

---
+++ IFIT/src/main/java/com/smhrd/ifit/controller/MemberController.java
@@ -0,0 +1,9 @@
+package com.smhrd.ifit.controller;
+
+public class MemberController {
+	
+	public static void main(String[] args) {
+		System.out.println("양진성");
+	}
+
+}
---
+++ IFIT/src/test/java/com/smhrd/ifit/IfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smhrd.ifit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
